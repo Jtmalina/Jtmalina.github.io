fcfa60fb7846a36f2626d6c122b7561c96a63e69
jtmalina <jtmalina@buffalo.edu>
2021-06-30 11:48:10 -0400

    Added navbar and pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Route, Link} from 'react-router-dom';
+import Portfolio from './Portfolio.js';
+import React, { Component} from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container'
+import Nav from 'react-bootstrap/Nav'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Footer from './components/Footer'
+import HomePage from './pages/HomePage'
+import AboutPage from './pages/AboutPage';
+
+class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state= {
+      title: 'Julian Malina',
+      headerLinks: [
+        {title: 'Home', path: '/' },
+        {title: 'About', path: '/about' }
+      ],
+      home: {
+        title: 'Welcome to Julians Portfolio',
+        subTitle: 'Projects from github below:'
+      },
+      about: {
+        title: 'Julian Malina',
+        subTitle: 'Graduated from Buffalo'
+      }
+    }
+  }
+  
+  render() {
+    return (
+      <BrowserRouter>
+        <Container className="p-0"fluid = {true}>
+          <Navbar className="border-bottom" bg="transparent" expand="lg">
+            <Navbar.Brand>Julian Malina</Navbar.Brand>
+            <Navbar.Toggle aria-controls="navbar-toggle"/>
+            <Navbar.Collapse id="navbar-toggle">
+              <Nav className="ml-auto">
+                <Link className="nav-link" to="/">Home</Link>
+                <Link className="nav-link" to="/about">About</Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+
+          <Route path="/" exact render={() => <HomePage title={this.state.home.title} subTitle={this.state.home.subTitle} />} /> 
+          {/* <Route path="/" exact render={() => <AboutPage title={this.state.home.title} subTitle={this.state.home.subTitle} text={this.state.home.text} />} />  */}
+
+          <Footer />
+
+        </Container>
+      </BrowserRouter>
+    );
+  }
 }
+
 
 export default App;
